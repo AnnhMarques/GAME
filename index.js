ffae22e43fab4bab91289a5d1db006ee3ba31166
Anna Marques <AnnaMarques@Scotts-Macbook.local>
2023-11-28 13:31:37 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const hide = document.querySelector("#hide");
+const descobrir = document.querySelector("#descobrir");
+const botao = document.querySelector("go");
+let resposta = document.querySelector("#resposta");
+const button = document.querySelector("#send");
+let goal;
+
+
+
+button.addEventListener("click", enviar);
+
+function enviar() {
+
+    goal = parseInt(hide.value);
+
+    if (goal >= 0 && goal <= 10) {
+        hide.value = " ";
+        go.disabled = false;
+    } else {
+        alert("Invalid Number.")
+        go.disabled = true;
+
+    }
+
+}
+
+go.addEventListener("click", answer);
+
+function answer() {
+
+    const valor2 = parseInt(descobrir.value);
+
+    if (goal === valor2)
+        resposta.innerHTML = "Congrats, you dif it!"
+
+    else
+        resposta.innerHTML = "You suck. Try again."
+
+}
